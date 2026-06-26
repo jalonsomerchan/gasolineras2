4075cf7ec431e17c3f95948ece7957c7e2c222d2
Jorge <jalonsomerchan@gmail.com>
2026-06-26 07:47:21 +0200

    chore: update API credentials and standardize font sizes across form components

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.GASOLINA_CONFIG = {
   apiBase: 'https://alon.one/api/gasolina2',
-  apiKey: '',
+  apiKey: 'alon_415a83ee37cc7934ad02561e511cb0cc4661ff4a7d735636',
   API_BASE_URL: 'https://alon.one/api/gasolina2',
-  API_KEY: ''
+  API_KEY: 'alon_415a83ee37cc7934ad02561e511cb0cc4661ff4a7d735636'
 };
